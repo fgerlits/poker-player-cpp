4e60cf99e8a066044040212be7e5410252936df3
Ferenc Gerlits <fgerlits@gmail.com>
2014-04-26 14:15:26 +0200

    aggressive heads-up

--- player.cpp
+++ player.cpp
@@ -2,7 +2,7 @@
 #include "GameState.h"
 #include <iostream>
 
-const char* Player::VERSION = "1.0";
+const char* Player::VERSION = "1.1";
 
 namespace {
 	bool isAllIn(const json::Value& player) {
@@ -11,10 +11,26 @@ namespace {
 		return (status == "active" && stack == 0);
 	}
 
+	bool isActive(const json::Value& player) {
+		std::string status = player["status"];
+		int stack = player["stack"].ToInt();
+		return status == "active";
+	}
+
 	int countAllIns(const std::vector<json::Value> players) {
 		int result = 0;
 		for (const auto& player : players) {
 			if (isAllIn(player)) {
+				++result;
+			}
+		}
+		return result;
+	}
+
+	int countActives(const std::vector<json::Value> players) {
+		int result = 0;
+		for (const auto& player : players) {
+			if (isActive(player)) {
 				++result;
 			}
 		}
@@ -29,10 +45,13 @@ int Player::betRequest(json::Value game_state)
 	Ranking ranking = gs.hole_cards_ranking();
 
 	std::vector<json::Value> otherPlayers = gs.otherPlayers();
+	int numberOfOtherPlayers = countActives(otherPlayers);
 	int numberOfAllIns = countAllIns(otherPlayers);
 
 	int result = 0;
 	if (ranking.isGood() && numberOfAllIns < 2) {
+		result = 10000;
+	} else if (numberOfOtherPlayers == 1) {
 		result = 10000;
 	}
 	std::cerr << "### result: " << result << std::endl;
