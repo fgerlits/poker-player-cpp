832cfc028a85a999187ff0bc145f30f3a120ae30
Ferenc Gerlits <fgerlits@gmail.com>
2014-04-26 13:36:42 +0200

    pairs only 8 and up

--- Ranking.cpp
+++ Ranking.cpp
@@ -12,15 +12,33 @@ namespace {
 	bool isPair(const std::string& one, const std::string& two) {
 		return one == two;
 	}
+
+	int value(const std::string& rank) {
+		if ("2" <= rank && rank <= "9") {
+			return rank[0] - '0';
+		} else if ("10" == rank) {
+			return 10;
+		} else if ("J" == rank) {
+			return 11;
+		} else if ("Q" == rank) {
+			return 12;
+		} else if ("K" == rank) {
+			return 13;
+		} else if ("A" == rank) {
+			return 14;
+		}
+	}
 }
 
 bool Ranking::isGood() {
-	json::Value card1 = cards[0];
-	json::Value card2 = cards[1];
+	std::string card1 = cards[0][RANK];
+	std::string card2 = cards[1][RANK];
+	int value1 = value(card1);
+	int value2 = value(card2);
 
-	if (isFaceCard(card1[RANK]) && isFaceCard(card2[RANK])) {
+	if (isFaceCard(card1) && isFaceCard(card2)) {
 		return true;
-	} else if (isPair(card1[RANK], card2[RANK])) {
+	} else if (isPair(card1, card2) && value1 >= 8) {
 		return true;
 	} else {
 		return false;
