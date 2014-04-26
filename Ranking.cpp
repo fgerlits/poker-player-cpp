304576b40f79f6d865b31ef6c8be633afb77e372
Ferenc Gerlits <fgerlits@gmail.com>
2014-04-26 12:32:07 +0200

    pairs

--- Ranking.cpp
+++ Ranking.cpp
@@ -8,11 +8,21 @@ namespace {
 	}
 	
 	const std::string RANK = "rank";
+
+	bool isPair(const std::string& one, const std::string& two) {
+		return one == two;
+	}
 }
 
 bool Ranking::isGood() {
 	json::Value card1 = cards[0];
 	json::Value card2 = cards[1];
 
-	return isFaceCard(card1[RANK]) || isFaceCard(card2[RANK]);
+	if (isFaceCard(card1[RANK]) || isFaceCard(card2[RANK])) {
+		return true;
+	} else if (isPair(card1[RANK], card2[RANK]) {
+		return true;
+	} else {
+		return false;
+	}
 }
--- player.cpp
+++ player.cpp
@@ -2,14 +2,14 @@
 #include "GameState.h"
 #include <iostream>
 
-const char* Player::VERSION = "Default C++ folding player";
+const char* Player::VERSION = "1.0";
 
 int Player::betRequest(json::Value game_state)
 {
 	//std::cerr << "###> " << game_state.GetType() << std::endl;
 	GameState gs(game_state);
 	Ranking ranking = gs.hole_cards_ranking();
-	return ranking.isGood() ? 10000 : 100;
+	return ranking.isGood() ? 10000 : 0;
 }
 
 void Player::showdown(json::Value game_state)
